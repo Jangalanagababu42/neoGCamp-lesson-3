77f11a5917f8d823a0e4e8a45ad06b3a284303ff
Jangalanagababu42 <60348426+Jangalanagababu42@users.noreply.github.com>
2022-10-27 21:22:39 +0530

    feat : Added Typing Animation
    Added Typing Animation

---
+++ dynamic.js
@@ -0,0 +1,40 @@
+const dynamicContent=document.querySelector("#heading-inverted");
+
+const phrases=["WebDeveloper...", "Mentor..." , "Student...", "Professional..."];
+let phraseIndex=0;
+let letterIndex=0;
+const typingSpeed=150;
+const erasingSpeed=75;
+
+function TypeNames(phrase){
+
+    if(letterIndex==phrase.length){
+       eraseNames();
+    }
+    else if(letterIndex<phrase.length){
+     dynamicContent.textContent += phrase.charAt(letterIndex);
+     letterIndex++;
+     setTimeout(TypeNames(phrase),typingSpeed);
+    }
+
+}
+
+function eraseNames(){
+if(letterIndex===-1){
+    phraseIndex=(phraseIndex+1)%phrases.length;
+    letterIndex=0;
+    TypeNames(phrases[phraseIndex])
+}
+else if (letterIndex > -1){
+    let updatedPharse="";
+    for(let index=0; index < letterIndex;index++){
+        updatedPharse += phrases[phraseIndex].charAt(index);
+    }
+    dynamicContent.textContent = updatedPharse;
+    letterIndex--;
+    setTimeout(eraseNames,erasingSpeed);
+}
+}
+
+
+TypeNames(phrases[0]);
